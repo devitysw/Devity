4786bce00e7a66f1337fbec5264254e6c1ac921b
dkorecko <22599403+rese-sw@users.noreply.github.com>
2023-08-17 11:15:33 +0200

    added check for e-mail configuration for mailkitoptions

--- Devity.Mailing/ServiceCollectionExtensions.cs
+++ Devity.Mailing/ServiceCollectionExtensions.cs
@@ -9,8 +9,18 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddDevityMailing<T>(this IServiceCollection services, ConfigurationManager configuration) where T : CommonMailService
     {
+        var emailSection = configuration.GetSection("Email");
+
+        if (!emailSection.Exists())
+            throw new Exception("Missing Email section in the appsettings.");
+
+        var emailOptions = emailSection.Get<MailKitOptions>();
+
+        if (emailOptions is null)
+            throw new Exception("The Email section in the appsettings should be in a MailKitOptions format.");
+
         return services
             .AddScoped<T>()
-            .AddMailKit(options => options.UseMailKit(configuration.GetSection("Email").Get<MailKitOptions>()));
+            .AddMailKit(options => options.UseMailKit(emailOptions));
     }
 }
