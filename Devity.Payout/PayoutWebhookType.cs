86792836acc9cdc1f283e52062892f9a7ff4993c
dkorecko <22599403+rese-sw@users.noreply.github.com>
2023-10-15 09:28:19 +0200

    finished payment gateway

---
+++ Devity.Payout/PayoutWebhookType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public enum PayoutWebhookType
+{
+    [Display(Name = "withdrawal.paid")]
+    WithdrawalPaid,
+    [Display(Name = "withdrawal.failed")]
+    WithdrawalFailed,
+    [Display(Name = "checkout.succeeded")]
+    CheckoutSucceeded,
+    [Display(Name = "checkout.expired")]
+    CheckoutExpired,
+}
